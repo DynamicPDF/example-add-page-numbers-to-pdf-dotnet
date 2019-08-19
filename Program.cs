42bddc6560b2bf920745a9c10e7469dea973d6ed
Andrew Cochran <andrew@dynamicpdf.com>
2019-08-19 16:04:42 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using ceTe.DynamicPDF;
+using ceTe.DynamicPDF.Merger;
+using ceTe.DynamicPDF.PageElements;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace example_add_page_numbers_to_pdf_dotnet
+{
+    // This example has two parts,
+    // 1. Creating PDF with page numbers from scratch
+    // 2. Adding Page numbers to an existing PDF document.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreatePDFWithPageNumbers();
+
+            AddPageNumbersToExistingPDF();
+        }
+
+        // This example uses the DynamicPDF Generator for .NET product.
+        // Import the ceTe.DynamicPDF namespace for the Document, Page and Template classes.
+        // Import the ceTe.DynamicPDF.PageElements namespace for the PageNumberingLabel class.
+        private static void CreatePDFWithPageNumbers()
+        {
+            //Create document object
+            Document document = new Document();
+
+            //Create a Template object and set it to the document
+            Template documentTemplate = new Template();
+            document.Template = documentTemplate;
+
+            //Add page numbering label to the template
+            documentTemplate.Elements.Add(new PageNumberingLabel("%%PR%%%%SP%% of %%ST%%", 0, 680, 512, 12, Font.Helvetica, 12, TextAlign.Center));
+
+            //Start a section with pagenumbering style in the document and add pages to it
+            document.Sections.Begin(NumberingStyle.RomanLowerCase);
+            document.Pages.Add(new Page()); //Page 1
+            document.Pages.Add(new Page()); //Page 2
+            document.Pages.Add(new Page()); //Page 3
+
+            //Start a section with pagenumbering style in the document and add pages to it
+            document.Sections.Begin(NumberingStyle.Numeric);
+            document.Pages.Add(new Page()); //Page 4
+            document.Pages.Add(new Page()); //page 5
+            document.Pages.Add(new Page()); //page 6
+            document.Pages.Add(new Page()); //page 7
+
+            //Start a section with pagenumbering style in the document and add pages to it
+            document.Sections.Begin(NumberingStyle.RomanLowerCase, "Appendix A - ");
+            document.Pages.Add(new Page()); //page 8
+            document.Pages.Add(new Page()); //page 9
+
+            document.Draw("output.pdf");
+        }
+
+        // This examples uses the DynamicPDF Merger for .NET Product
+        // Import the ceTe.DynamicPDF namespace for the Template class
+        // Import the ceTe.DynamicPDF.PageElements namespace for the PageNumberingLabel element.
+        // Import the ceTe.DynamicPDF.Merger namespace for the MergeDocument class.
+        private static void AddPageNumbersToExistingPDF()
+        {
+            //Create a MergeDocument object using the existing PDF
+            MergeDocument document = new MergeDocument(GetResourcePath("doc-a.pdf"));
+
+            //Create a template bject
+            Template template = new Template();
+
+            //Create a PageNumberingLabel and add it to the template
+            PageNumberingLabel pageLabels = new PageNumberingLabel("%%CP%% of %%TP%%", 0, 0, 200, 20);
+            template.Elements.Add(pageLabels);
+
+            //Set template to the document
+            document.Template = template;
+
+            //Save document.
+            document.Draw("outputFromExistingPDF.pdf");
+        }
+
+        // This is a helper function to get a full path to a file in the Resources folder.
+        public static string GetResourcePath(string inputFileName)
+        {
+            var exePath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+            Regex appPathMatcher = new Regex(@"(?<!fil)[A-Za-z]:\\+[\S\s]*?(?=\\+bin)");
+            var appRoot = appPathMatcher.Match(exePath).Value;
+            return System.IO.Path.Combine(appRoot, "Resources", inputFileName);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+﻿# Add Page Numbers to a PDF Example for C# (.NET Core)
+
+This example shows how to add page numbers to a new and an existing PDF document. More details can be found
+[https://www.DynamicPDF.com/Examples/add-page-numbers-to-pdf-.net-core](here).
